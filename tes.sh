2adf53b41beca0ef63890e7b4d744a0f5843167a
ramdani223 <139839967+ramdani223@users.noreply.github.com>
2023-07-18 11:25:41 +0700

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,13 @@
+pkg install automake clang git nano
+ 
+y
+ 
+git clone https://github.com/decryp2kanon/sugarmaker 
+ 
+cd sugarmaker 
+./autogen.sh
+./configure CFLAGS="-Wall -O2 -fomit-frame-pointer" CXXFLAGS="$CFLAGS -std=gnu++11"
+ 
+ make
+ 
+./sugarmaker -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qfrq5zfyz9ktxfpx6m0qwlm8rwn0jx9nvh2tgnu -t 4
