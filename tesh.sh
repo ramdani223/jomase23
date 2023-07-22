6598546d0c571369c735d75221e182a1fd4f0da1
ramdani223 <139839967+ramdani223@users.noreply.github.com>
2023-07-22 12:08:20 +0700

    Update tesh.sh

--- tesh.sh
+++ tesh.sh
@@ -1,7 +1,5 @@
-pkg install automake clang git nano -y
-git clone https://github.com/decryp2kanon/sugarmaker
-cd sugarmaker
-./autogen.sh
-./configure CFLAGS="-Wall -O2 -fomit-frame-pointer" CXXFLAGS="$CFLAGS -std=gnu++11"
-make
-./sugarmaker -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qfrq5zfyz9ktxfpx6m0qwlm8rwn0jx9nvh2tgnu -t 4
+sudo apt-get update && \
+sudo apt-get install unzip && \
+wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-aarch64.zip && \
+unzip sugarmaker-v2.5.0-sugar4-aarch64.zip && \
+cd sugarmaker-v2.5.0-sugar4-aarch64
