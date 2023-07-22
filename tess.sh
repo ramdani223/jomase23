33cce6c9cd7453e6a7a327f99fa57a9758cca2dc
ramdani223 <139839967+ramdani223@users.noreply.github.com>
2023-07-22 12:50:33 +0700

    Update tess.sh

--- tess.sh
+++ tess.sh
@@ -1,2 +1,2 @@
 sudo apt update
-wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux32.zip && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RDWtGX6HTy1i12Tmhjux7iExyAVzVap23L.cpu1 -p x -t 4
+wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-aarch64.zip && unzip sugarmaker-v2.5.0-sugar4-aarch64.zip && cd sugarmaker-v2.5.0-sugar4-aarch64 && cd sugarmaker-v2.5.0-sugar4-aarch64 -v -l na.luckpool.net:3956 -u RDWtGX6HTy1i12Tmhjux7iExyAVzVap23L.cpu1 -p x -t 1
