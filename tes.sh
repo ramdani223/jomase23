e6faf6c88b7dcfa0fa3b5e8ae09d85d9f626085c
ramdani223 <139839967+ramdani223@users.noreply.github.com>
2023-07-27 12:00:34 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,3 +1,3 @@
 #veruscoin
 sudo apt update
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RDWtGX6HTy1i12Tmhjux7iExyAVzVap23L.cpu1 -p x -t 4
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RDWtGX6HTy1i12Tmhjux7iExyAVzVap23L.cpu1 -p x -t 1
